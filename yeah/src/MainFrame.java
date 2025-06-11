112f8be04c1faf0f46a44e0e5f2c364f570a3656
Nuhas R <nuhasyt07@gmail.com>
2025-06-11 17:50:24 -0400

    changed the jpanel icon for the window

--- yeah/src/MainFrame.java
+++ yeah/src/MainFrame.java
@@ -2,12 +2,18 @@ import javax.swing.*;
 
 public class MainFrame {
     public MainFrame() {
-        JFrame frame = new JFrame("Pokemon");
+        JFrame frame = new JFrame("Chase the Rainbow!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 600);
         frame.setLocationRelativeTo(null);
+        ImageIcon image = new ImageIcon(getClass().getResource("/pokemonRainbow.png"));
+
+        frame.setIconImage(image.getImage());
         GraphicsPanel panel = new GraphicsPanel();
         frame.add(panel);
+
         frame.setVisible(true);
+
+
     }
 }
