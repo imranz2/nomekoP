b658455bcaaee2096169134b7ac223a9fa6f6113
Nuhas R <nuhasyt07@gmail.com>
2025-06-11 12:51:00 -0400

    deleted the pokemon class to suit the new project direction

--- yeah/src/GraphicsPanel.java
+++ yeah/src/GraphicsPanel.java
@@ -12,7 +12,7 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
     private BufferedImage idle;
     private Timer timer;
     private boolean[] pressedKeys;
-    private ArrayList<Pokemon> pokemon = new ArrayList<>(); //All pokemon 150+;
+   // private ArrayList<Pokemon> pokemon = new ArrayList<>(); //All pokemon 150+;
     private Player player;
     private Rectangle character;
     private int xBack = -780;
@@ -55,9 +55,9 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        pokemon.add(new Pokemon("charizard", "fire", 170, "Flamethrower", 40, "Focus Blast", 90, "Roar", 30)); //90 dmg with 50% of missing, All jhave chanceo f missing except Scratch
-        pokemon.add(new Pokemon("greninja", "water", 130, "Water Shuriken", 50, "Water Gun", 0, "Tackle", 25));
-        pokemon.add(new Pokemon("goku", "everything", 1000, "Kamehameha", 75, "Kaioken", 0, "Spirit Bomb", 500)); // Spirit bomb with 10% chance, Kmehame with75% chance
+//        pokemon.add(new Pokemon("charizard", "fire", 170, "Flamethrower", 40, "Focus Blast", 90, "Roar", 30)); //90 dmg with 50% of missing, All jhave chanceo f missing except Scratch
+//        pokemon.add(new Pokemon("greninja", "water", 130, "Water Shuriken", 50, "Water Gun", 0, "Tackle", 25));
+//        pokemon.add(new Pokemon("goku", "everything", 1000, "Kamehameha", 75, "Kaioken", 0, "Spirit Bomb", 500)); // Spirit bomb with 10% chance, Kmehame with75% chance
         pressedKeys = new boolean[128];
         addKeyListener(this);
         addMouseListener(this);
